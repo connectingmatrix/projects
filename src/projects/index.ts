98cbcacf4f87dc98ec6e31a6a3e1c66ce38aa531
Abeer Remote Agent <abeer+remote-agent@connectingmatrix.com>
2026-05-20 21:24:46 +0500

    chore: checkpoint commit via tree script

--- src/projects/index.ts
+++ src/projects/index.ts
@@ -1,4 +1,4 @@
 export * from '@connectingmatrix/orm/entities/AIAgentProjectEntity';
-export * from '@connectingmatrix/orm/entities/SharedSpaceEntity';
 export * from '@connectingmatrix/orm/entities/SharedSpaceCatalogEntity';
+export * from '@connectingmatrix/file-service/shared-space';
 export * from './source-bundle';
